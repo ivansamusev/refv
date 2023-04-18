0ce921ea50f785ea493e046a6f7c2141e724b58f
ivan <ivansamusev044@gmail.com>
2023-04-18 17:56:47 +0300

    galery

--- src/js/renderHTMl.js
+++ src/js/renderHTMl.js
@@ -1,23 +1,98 @@
+import { galleryItems } from './imgs';
+
 function createLi(email, password) {
-  const markup = `
-    <li>Email: ${email} , password: ${password}
-    </li>
-    `;
+  const markup = `<li> Email: ${email}, Password: ${password}</li>`;
   return markup;
 }
 
-function createLiStorage(VALUES_KEY) {
+function createLiFromStorage(VALUES_KEY) {
   if (localStorage.getItem(VALUES_KEY)) {
     const values = localStorage.getItem(VALUES_KEY);
     const parseValue = JSON.parse(values);
 
-   const itemMap = parseValue.map(({ email, password }) => {
-      const markupMap = `
-    <li>Email: ${email}, password: ${password}
-    </li>`;
-      return markupMap;
-    });
-    return itemMap
+    const item = parseValue
+      .map(({ email, password }) => {
+        const markupMap = `<li> Email: ${email}, Password: ${password}</li>`;
+
+        return markupMap;
+      })
+      .join(' ');
+    return item;
   }
+  console.log(item);
 }
-export { createLi, createLiStorage };
+
+function createLiImg(filterValue) {
+  if (filterValue) {
+    return galleryItems
+      .filter(({ filter }) => filter === filterValue)
+      .map(({ preview, original, description }) => {
+        return `<li class="galleryItem">
+              <a class="gallery-link" href="${original}">
+                <img class="gallery-img" src="${preview}" alt="${description}" data-sourse="${original}"  >
+              </a>
+            </li>`;
+      })
+      .join(' ');
+  }
+
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="galleryItem">
+              <a class="gallery-link" href="${original}">
+                <img class="gallery-img" src="${preview}" alt="${description}" data-sourse="${original}"  >
+              </a>
+            </li>`;
+    })
+    .join(' ');
+}
+    
+function createFilters() {
+  const filtersObj = galleryItems.map(({ filter }) => filter);
+  const unique = [...new Set(filtersObj)];
+  const filter = unique
+    .map(item => {
+      return `<li class="filter-item">
+             ${item}
+            </li>`;
+    })
+    .join(' ');
+  return filter;
+}
+
+function createCountrys(countrys) {
+  const markUp = countrys
+    .map(
+      ({ name, capital, flags }) =>
+        `<li class="">
+        <p> <img src="${flags.png}" alt="${flags.alt}" width="30"> ${name.common}</p>
+   <p>${capital}</p>
+   </li>`
+    )
+    .join(' ');
+
+  return markUp;
+}
+
+function createCountry(country) {
+  const markUp = country
+    .map(
+      ({ currencies, population, languages }) => `<div class="">
+    <p>currencies --->  ${[...Object.keys(currencies)]} </p>
+    <p>population --->  ${population} </p>
+    <p>languages --->  ${[...Object.values(languages)]} </p>
+ </div>`
+    )
+    .join(' ');
+
+  return markUp;
+}
+
+export {
+  createLi,
+  createLiFromStorage,
+  createLiImg,
+  createFilters,
+  createCountrys,
+  createCountry,
+};
---
+++ src/views/galery.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { createLiImg, createFilters } from '../js/renderHtml';
+
+const refs = {
+  filters: document.querySelector('.filters'),
+  list: document.querySelector('.list'),
+};
+
+refs.filters.insertAdjacentHTML('beforeend', createFilters());
+refs.list.insertAdjacentHTML('beforeend', createLiImg());
+
+refs.filters.addEventListener('click', filter);
+
+function filter(e) {
+  e.preventDefault();
+  const filterValue = e.target.textContent.trim();
+
+  if (e.target.nodeName === 'LI') {
+    e.target.classList.add('active');
+    refs.list.innerHTML = '';
+
+    refs.list.insertAdjacentHTML('beforeend', createLiImg(filterValue));
+  }
+
+  const filterElem = refs.filters.querySelectorAll('li');
+
+  filterElem.forEach(item => {
+    if (item.textContent.trim() !== filterValue) {
+      item.classList.remove('active');
+    }
+  });
+  if (e.target.nodeName !== 'LI') {
+    refs.list.innerHTML = '';
+    refs.list.insertAdjacentHTML('beforeend', createLiImg());
+  }
+}
+let lightbox = new SimpleLightbox('.list a', {
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
